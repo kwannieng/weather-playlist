049f2f1ba65bda55b795584d7ab94fd6bd7e87d1
osboxes.org <milktealliance@protonmail.com>
2023-03-20 19:42:36 +0000

    change

--- src/server/server.js
+++ src/server/server.js
@@ -8,5 +8,5 @@ app.get('/',(req,res)=>{
 })
 
 app.listen(process.env.PORT,()=>{
-    console.log('listening on port',process.env.PORT);
+    console.log('listening on port!!!!!',process.env.PORT);
 })
